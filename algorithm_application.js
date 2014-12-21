29f386ae1fd4a425b81b758bfc3eae3927730182
rodrigo <rodrigo@temporadafora.com>
2014-12-21 03:33:07 -0500

    initial version of the market maker algorithm

--- algorithm_application.js
+++ algorithm_application.js
@@ -9,18 +9,17 @@ goog.require('goog.array');
 goog.require('goog.object');
 
 
-
-
 /**
  * @param {string} instance_id
  * @param {string} websocket_url
  * @param {string} symbol
  * @param {Object.<number,Object.<string, number|string>>} open_orders
  * @param {Object.<string,string|number|Object>} algorithm_definition
+ * @param {Object.<string,number>} balance
  * @param {function(Application,string): AlgorithmTradingInterface} fn_creator
  * @constructor
  */
-var Application = function( instance_id, websocket_url, symbol, open_orders, algorithm_definition, fn_creator ) {
+var Application = function( instance_id, websocket_url, symbol, open_orders, algorithm_definition,balance, fn_creator ) {
   this.websocket_url_ = websocket_url;
   this.instance_id_ = instance_id;
   this.selected_symbol_ = symbol;
@@ -34,7 +33,7 @@ var Application = function( instance_id, websocket_url, symbol, open_orders, alg
   this.status_started_ = false;
   this.trade_history_ = [];
   this.order_book_ = {};
-  this.balance_ = {};
+  this.balance_ = balance;
 
   this.ws_ = new WebSocket(this.websocket_url_);
 
@@ -72,36 +71,6 @@ Application.prototype.sendBuyLimitedOrder = function( qty, price, opt_clientOrde
   return clientOrderId;
 };
 
-/**
- * Cancel an order.  You must pass opt_clientOrderId nor opt_orderId
- * @param {number|string=} opt_clientOrderId Defaults to random generated number
- * @param {number=} opt_orderId
- */
-Application.prototype.cancelOrder = function( opt_clientOrderId, opt_orderId ) {
-  if (!goog.isDefAndNotNull(opt_clientOrderId) && !(goog.isDefAndNotNull(opt_orderId))){
-    this.stop('Invalid paramaters. Missing opt_clientOrderId or opt_orderId');
-    return;
-  }
-
-  if (goog.isDefAndNotNull(opt_clientOrderId) && (goog.isDefAndNotNull(opt_orderId))){
-    this.stop('Invalid paramaters. You must passa either opt_clientOrderId or opt_orderId');
-    return;
-  }
-
-  postMessage({ 'rep':'cancel_order',
-                'instance':this.instance_id_,
-                'client_order_id': opt_clientOrderId,
-                'order_id': opt_orderId
-              });
-};
-
-/**
- * Cancel all orders.
- */
-Application.prototype.cancelAllOrders = function() {
-  postMessage({ 'rep':'cancel_order', 'instance':this.instance_id_ });
-};
-
 
 /**
  * Send a sell order
@@ -123,6 +92,49 @@ Application.prototype.sendSellLimitedOrder = function( qty, price, opt_clientOrd
 
   return clientOrderId;
 };
+
+
+/**
+ * Cancel an order.  You must pass opt_clientOrderId nor opt_orderId
+ * @param {number|string=} opt_clientOrderId Defaults to random generated number
+ * @param {number=} opt_orderId
+ */
+Application.prototype.cancelOrder = function( opt_clientOrderId, opt_orderId ) {
+  if (!goog.isDefAndNotNull(opt_clientOrderId) && !(goog.isDefAndNotNull(opt_orderId))){
+    this.stop('Invalid paramaters. Missing opt_clientOrderId or opt_orderId');
+    return;
+  }
+
+  if (goog.isDefAndNotNull(opt_clientOrderId) && (goog.isDefAndNotNull(opt_orderId))){
+    this.stop('Invalid paramaters. You must passa either opt_clientOrderId or opt_orderId');
+    return;
+  }
+
+  if (goog.isDefAndNotNull(opt_clientOrderId)) {
+    goog.object.remove(this.open_orders_, /** @type {string} */(opt_clientOrderId));
+  } else if (goog.isDefAndNotNull(opt_orderId)) {
+    var client_order_id = goog.object.findKey( this.open_orders_, function( order ){
+      return (order['OrderID'] ==  opt_orderId);
+    });
+    if (goog.isDefAndNotNull(client_order_id)) {
+      goog.object.remove(this.open_orders_, /** @type {string} */(client_order_id));
+    }
+  }
+
+  postMessage({ 'rep':'cancel_order',
+                'instance':this.instance_id_,
+                'client_order_id': opt_clientOrderId,
+                'order_id': opt_orderId
+              });
+};
+
+/**
+ * Cancel all orders.
+ */
+Application.prototype.cancelAllOrders = function() {
+  postMessage({ 'rep':'cancel_order', 'instance':this.instance_id_ });
+};
+
 
 
 /**
@@ -148,10 +160,19 @@ Application.prototype.getBalance = function( currency, type ) {
   if (type == "deposit") {
     return this.balance_[currency];
 
-  } if (type == "available") {
-    return this.balance_[currency] - this.balance_[currency + '_locked'];
+  } else if (type == "available") {
+    if (goog.isDefAndNotNull(this.balance_[currency + '_locked'])) {
+      return this.balance_[currency] - this.balance_[currency + '_locked'];
+    } else if (goog.isDefAndNotNull(this.balance_[currency])) {
+      return this.balance_[currency];
+    } else {
+      return 0;
+    }
+  } else if (goog.isDefAndNotNull(type)) {
+    return this.balance_[currency + '_' + type];
+  } else {
+    return this.balance_[currency];
   }
-  return this.balance_[currency + '_' + type];
 };
 
 
@@ -292,7 +313,7 @@ Application.prototype.onWebSocketOpen_ = function(e) {
   };
   this.ws_.send(JSON.stringify(msgSubscribeSecurityStatus));
 
-  setTimeout( goog$bind(this.sendTestRequest_, this), 30000);
+  setTimeout( goog.bind(this.sendTestRequest_, this), 30000);
 };
 
 Application.prototype.sendTestRequest_ = function() {
@@ -310,6 +331,7 @@ Application.prototype.start_ = function(params) {
   }
 
   try {
+    this.params_ = params;
     this.instance_.start(params);
     this.status_started_ = true;
   } catch(e) {}
@@ -369,9 +391,16 @@ Application.prototype.processParamsMsg_ = function(msg) {
  */
 Application.prototype.processExecutionReportMsg_ = function(msg) {
   if (msg['OrdStatus'] == '2' || msg['OrdStatus'] == '4' ) {
-    delete this.open_orders_[msg['OrderID'] ];
+    goog.object.remove(this.open_orders_, /** @type {string} */(msg['ClOrdID'] ));
   } else {
-    this.open_orders_[msg['OrderID'] ] = msg;
+    if (msg['OrdStatus'] == 'A') {
+      this.open_orders_[msg['ClOrdID'] ] = msg;
+    } else {
+      if (msg['OrdStatus'] == '0'){
+        goog.object.remove(this.open_orders_, /** @type {string} */(msg['ClOrdID'] ));
+      }
+      this.open_orders_[msg['ClOrdID'] ] = msg;
+    }
   }
 
   try {
@@ -418,7 +447,7 @@ Application.prototype.onMDNewOrder_ = function(msg) {
   }
 
   try {
-    this.instance_.onOrderBookNewOrder(entry);
+    this.instance_.onOrderBookNewOrder(msg);
   } catch(e) {}
 
   try {
@@ -603,6 +632,7 @@ addEventListener('message', function(e) {
                                context["symbol"],
                                context["open_orders"],
                                context["algo_definition"],
+                               context["balance"],
                                creator_fn);
         break;
       case 'start':
@@ -629,6 +659,118 @@ addEventListener('message', function(e) {
     self.close();
   }
 }, false);
+
+
+goog.exportSymbol('goog.bind', goog.bind);
+goog.exportSymbol('goog.isDefAndNotNull', goog.isDefAndNotNull);
+goog.exportSymbol('goog.typeOf',goog.typeOf);
+goog.exportSymbol('goog.isDef',goog.isDef);
+goog.exportSymbol('goog.isNull',goog.isNull);
+goog.exportSymbol('goog.isArray',goog.isArray);
+goog.exportSymbol('goog.isArrayLike',goog.isArrayLike);
+goog.exportSymbol('goog.isDateLike',goog.isDateLike);
+goog.exportSymbol('goog.isString',goog.isString);
+goog.exportSymbol('goog.isBoolean',goog.isBoolean);
+goog.exportSymbol('goog.isNumber',goog.isNumber);
+goog.exportSymbol('goog.isFunction',goog.isFunction);
+goog.exportSymbol('goog.isObject',goog.isObject );
+goog.exportSymbol('goog.cloneObject',goog.cloneObject);
+goog.exportSymbol('goog.partial',goog.partial);
+goog.exportSymbol('goog.mixin',goog.mixin);
+goog.exportSymbol('goog.now',goog.now);
+goog.exportSymbol('goog.globalEval',goog.globalEval);
+goog.exportSymbol('goog.inherits',goog.inherits);
+goog.exportSymbol('goog.base',goog.base);
+
+
+
+
+goog.exportSymbol('goog.array.splice', goog.array.splice);
+goog.exportSymbol('goog.array.insertAt', goog.array.insertAt);
+goog.exportSymbol('goog.array.indexOf', goog.array.indexOf);
+goog.exportSymbol('goog.array.lastIndexOf',goog.array.lastIndexOf);
+goog.exportSymbol('goog.array.forEach',goog.array.forEach);
+goog.exportSymbol('goog.array.forEachRight',goog.array.forEachRight);
+goog.exportSymbol('goog.array.filter',goog.array.filter);
+goog.exportSymbol('goog.array.map',goog.array.map);
+goog.exportSymbol('goog.array.reduce',goog.array.reduce);
+goog.exportSymbol('goog.array.reduceRight',goog.array.reduceRight);
+goog.exportSymbol('goog.array.some',goog.array.some);
+goog.exportSymbol('goog.array.every',goog.array.every);
+goog.exportSymbol('goog.array.count',goog.array.count);
+goog.exportSymbol('goog.array.findIndex',goog.array.findIndex);
+goog.exportSymbol('goog.array.findRight',goog.array.findRight);
+goog.exportSymbol('goog.array.findIndexRight',goog.array.findIndexRight);
+goog.exportSymbol('goog.array.contains',goog.array.contains);
+goog.exportSymbol('goog.array.isEmpty',goog.array.isEmpty);
+goog.exportSymbol('goog.array.clear',goog.array.clear);
+goog.exportSymbol('goog.array.insert',goog.array.insert);
+goog.exportSymbol('goog.array.insertArrayAt',goog.array.insertArrayAt);
+goog.exportSymbol('goog.array.insertBefore',goog.array.insertBefore);
+goog.exportSymbol('goog.array.remove',goog.array.remove);
+goog.exportSymbol('goog.array.removeAt',goog.array.removeAt);
+goog.exportSymbol('goog.array.removeIf',goog.array.removeIf);
+goog.exportSymbol('goog.array.concat',goog.array.concat);
+goog.exportSymbol('goog.array.toArray',goog.array.toArray);
+goog.exportSymbol('goog.array.clone',goog.array.clone);
+goog.exportSymbol('goog.array.extend',goog.array.extend);
+goog.exportSymbol('goog.array.slice',goog.array.slice);
+goog.exportSymbol('goog.array.removeDuplicates',goog.array.removeDuplicates);
+goog.exportSymbol('goog.array.binarySearch',goog.array.binarySearch);
+goog.exportSymbol('goog.array.binarySelect',goog.array.binarySelect);
+goog.exportSymbol('goog.array.sort',goog.array.sort);
+goog.exportSymbol('goog.array.stableSort',goog.array.stableSort);
+goog.exportSymbol('goog.array.sortObjectsByKey',goog.array.sortObjectsByKey);
+goog.exportSymbol('goog.array.isSorted',goog.array.isSorted);
+goog.exportSymbol('goog.array.equals',goog.array.equals);
+goog.exportSymbol('goog.array.compare3',goog.array.compare3);
+goog.exportSymbol('goog.array.defaultCompare',goog.array.defaultCompare);
+goog.exportSymbol('goog.array.defaultCompareEquality',goog.array.defaultCompareEquality);
+goog.exportSymbol('goog.array.binaryInsert',goog.array.binaryInsert);
+goog.exportSymbol('goog.array.binaryRemove',goog.array.binaryRemove);
+goog.exportSymbol('goog.array.bucket',goog.array.bucket);
+goog.exportSymbol('goog.array.toObject',goog.array.toObject);
+goog.exportSymbol('goog.array.range',goog.array.range);
+goog.exportSymbol('goog.array.repeat',goog.array.repeat);
+goog.exportSymbol('goog.array.flatten',goog.array.flatten);
+goog.exportSymbol('goog.array.rotate',goog.array.rotate);
+goog.exportSymbol('goog.array.zip',goog.array.zip)
+goog.exportSymbol('goog.array.shuffle',goog.array.shuffle);
+
+
+
+goog.exportSymbol('goog.object.forEach', goog.object.forEach);
+goog.exportSymbol('goog.object.extend', goog.object.extend);
+goog.exportSymbol('goog.object.filter',goog.object.filter);
+goog.exportSymbol('goog.object.map',goog.object.map);
+goog.exportSymbol('goog.object.some',goog.object.some);
+goog.exportSymbol('goog.object.every',goog.object.every);
+goog.exportSymbol('goog.object.getCount',goog.object.getCount);
+goog.exportSymbol('goog.object.getAnyKey',goog.object.getAnyKey);
+goog.exportSymbol('goog.object.getAnyValue',goog.object.getAnyValue);
+goog.exportSymbol('goog.object.contains',goog.object.contains);
+goog.exportSymbol('goog.object.getValues',goog.object.getValues);
+goog.exportSymbol('goog.object.getKeys',goog.object.getKeys);
+goog.exportSymbol('goog.object.getValueByKeys',goog.object.getValueByKeys);
+goog.exportSymbol('goog.object.containsKey',goog.object.containsKey);
+goog.exportSymbol('goog.object.containsValue',goog.object.containsValue);
+goog.exportSymbol('goog.object.findKey',goog.object.findKey);
+goog.exportSymbol('goog.object.findValue',goog.object.findValue);
+goog.exportSymbol('goog.object.isEmpty',goog.object.isEmpty);
+goog.exportSymbol('goog.object.clear',goog.object.clear);
+goog.exportSymbol('goog.object.remove',goog.object.remove);
+goog.exportSymbol('goog.object.add',goog.object.add);
+goog.exportSymbol('goog.object.get',goog.object.get);
+goog.exportSymbol('goog.object.set',goog.object.set);
+goog.exportSymbol('goog.object.setIfUndefined',goog.object.setIfUndefined);
+goog.exportSymbol('goog.object.clone',goog.object.clone);
+goog.exportSymbol('goog.object.unsafeClone',goog.object.unsafeClone);
+goog.exportSymbol('goog.object.transpose',goog.object.transpose);
+goog.exportSymbol('goog.object.create',goog.object.create);
+goog.exportSymbol('goog.object.createSet',goog.object.createSet);
+goog.exportSymbol('goog.object.createImmutableView',goog.object.createImmutableView);
+goog.exportSymbol('goog.object.isImmutableView',goog.object.isImmutableView);
+
 
 
 goog.exportSymbol('Application', Application);
